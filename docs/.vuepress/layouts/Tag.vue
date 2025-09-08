492603c3faada210fc1e61d0e803d687fbefe0f6
yevachenchen <yevachenchen@didiglobal.com>
2025-09-08 21:08:07 +0800

    feat: css.md

--- docs/.vuepress/layouts/Tag.vue
+++ docs/.vuepress/layouts/Tag.vue
@@ -3,9 +3,29 @@ import { useBlogCategory } from '@vuepress/plugin-blog/client'
 import ParentLayout from '@vuepress/theme-default/layouts/Layout.vue'
 import { RouteLink, useRoutePath } from 'vuepress/client'
 import ArticleList from '../components/ArticleList.vue'
+import { computed } from 'vue'
 
 const tagMap = useBlogCategory('tag')
 const routePath = useRoutePath()
+
+// 为每个 tag 生成基于名称的稳定随机颜色
+const getTagColor = (tagName) => {
+  // 使用 tag 名称作为种子生成稳定的随机数
+  let hash = 0
+  for (let i = 0; i < tagName.length; i++) {
+    const char = tagName.charCodeAt(i)
+    hash = ((hash << 5) - hash) + char
+    hash = hash & hash // 转换为32位整数
+  }
+  
+  // 使用哈希值生成 RGB 颜色
+  const r = Math.abs(hash) % 151 + 50
+  const g = Math.abs(hash >> 8) % 151 + 50
+  const b = Math.abs(hash >> 16) % 151 + 50
+  
+  return `rgb(${r}, ${g}, ${b})`
+}
+
 </script>
 
 <template>
@@ -18,6 +38,7 @@ const routePath = useRoutePath()
             :key="name"
             :to="path"
             :active="routePath === path"
+            :style="{ backgroundColor: getTagColor(name) }"
             class="tag"
           >
             {{ name }}
@@ -39,9 +60,11 @@ const routePath = useRoutePath()
 .tag-wrapper {
   @include mixins.content-wrapper;
 
-  padding-top: calc(var(--navbar-height) + 1rem) !important;
+  margin-top: calc(var(--navbar-height) + 1rem) !important;
   padding-bottom: 0 !important;
   font-size: 0.875em;
+  width: fit-content;
+  height: fit-content;
 
   .route-link {
     color: inherit;
